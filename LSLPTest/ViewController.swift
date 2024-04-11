f3b78cd7c1da073663a5158afbee7f6a52a958a5
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-04-11 16:59:46 +0900

    Initial Commit

---
+++ LSLPTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSLPTest
+//
+//  Created by 최서경 on 4/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
